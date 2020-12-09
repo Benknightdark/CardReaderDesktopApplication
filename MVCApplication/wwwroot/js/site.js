51317cefcfd22180162cd1c37db29438d75435ce
ben_kung <ben_kung@weleader.com.tw>
2020-12-09 10:40:20 +0800

    dddd

--- MVCApplication/wwwroot/js/site.js
+++ MVCApplication/wwwroot/js/site.js
@@ -15,14 +15,28 @@ import { customAlert } from './helpers/AlertHelper.js'
     })
 
     console.log(alertResult)
-    
+
     $('#fuckBtn').on('click', async function () {
         try {
             const req = await fetch('http://localhost:59487/api/cardreader')
             const res = await req.json()
-            alert(JSON.stringify(res))
+            const alertResult = await customAlert({
+                title: 'Your card Info',
+                text: JSON.stringify(res),
+                icon: 'info',
+                showCancelButton: true,
+                confirmButtonText: 'OK',
+                cancelButtonText: 'Cancel'
+            })
         } catch (e) {
             alert(e)
+            const alertResult = await customAlert({
+                title: 'Error',
+                text: e,
+                icon: 'info',
+                showCancelButton: false,
+                confirmButtonText: 'OK',
+            })
         }
     })
 })($, document, window)
