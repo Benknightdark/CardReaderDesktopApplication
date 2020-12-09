9ea7e751813c703b6a870fc355f0db5dccc7cda2
ben_kung <ben_kung@weleader.com.tw>
2020-12-09 10:37:51 +0800

    OK

---
+++ MVCApplication/wwwroot/js/helpers/AlertHelper.js
@@ -0,0 +1,18 @@
+﻿/**
+ *  Swal.fire Alert Dialog -
+ * @param {*} data -
+ * 
+ *  ```
+ *  {
+ *       title: 'Are you sure?',
+ *       text: 'You will not be able to recover this imaginary file!',
+ *       icon: 'warning',
+ *       showCancelButton: true,
+ *       confirmButtonText: 'Yes, delete it!',
+ *       cancelButtonText: 'No, keep it'
+ *   }
+ * ```
+ */
+export const customAlert = async  (data) => {
+    return Swal.fire(data)
+}
--- MVCApplication/wwwroot/js/site.js
+++ MVCApplication/wwwroot/js/site.js
@@ -1,4 +1,28 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+import { customAlert } from './helpers/AlertHelper.js'
 // Write your JavaScript code.
+(async ($, document, window) => {
+
+    const alertResult = await customAlert({
+        title: 'Are you sure?',
+        text: 'You will not be able to recover this imaginary file!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, keep it'
+    })
+
+    console.log(alertResult)
+    
+    $('#fuckBtn').on('click', async function () {
+        try {
+            const req = await fetch('http://localhost:59487/api/cardreader')
+            const res = await req.json()
+            alert(JSON.stringify(res))
+        } catch (e) {
+            alert(e)
+        }
+    })
+})($, document, window)
