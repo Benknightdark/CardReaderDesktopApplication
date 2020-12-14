38b22e3a088c5bedb427b58554f89865990fea49
ben_kung <ben_kung@weleader.com.tw>
2020-12-14 16:38:21 +0800

    add app.config

--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -28,6 +29,8 @@ namespace WindowsFormsApp
             {
                 Program.CreateHostBuilder().Build().Run();
             });
+            string DBname = ConfigurationManager.AppSettings["DB"];
+
             this.WindowState = FormWindowState.Maximized;
             //https://5f5460623372.ngrok.io/
             //http://localhost:4000
